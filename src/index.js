52b07c862ceb307bedd1ccd0b3f1fe94af39f1b4
Hyeonnam-J <nami0879@naver.com>
2024-05-24 14:53:55 +0900

    complete one to many, existed console.log and additional logic

--- src/index.js
+++ src/index.js
@@ -10,6 +10,8 @@ const client = new net.Socket();
 const inputContent = document.getElementById('inputContent');
 const sendButton = document.getElementById('sendButton');
 
+const messages = [];
+
 // 앱을 실행하면 바로 연결. 로그인 기능 생략.
 client.connect(PORT, HOST, () => {
     let m_id = "-1";
@@ -18,24 +20,31 @@ client.connect(PORT, HOST, () => {
         const json_data = data.toString();
         const obj_data = JSON.parse(json_data);
 
-        // 다른 방법이...
-        if (obj_data.infoType === Chat.infoType.alaram) {  // - 입장인 경우,
-            // 입장 시 서버가 단독으로 보낸 메시지.
-            if (obj_data.infoOrigin === Chat.infoOrigin.self) {
-                m_id = obj_data.id;
-                console.log('id가 발급되었습니다: ', m_id, ' / ', obj_data.message);
-            } else {
-                // 다른 유저의 입장 알림
-                console.log(`${obj_data.id}님이 대화방에 입장하셨습니다 !`);
+        if(obj_data.issued){
+            // 나 입장 시, 메시지 목록에 추가하지 않고, 아이디 발급 로직.
+            // 서버가 접속한 소켓에게만 단독으로 보낸 메시지.
+            m_id = obj_data.id;
+            console.log('id가 발급되었습니다: ', m_id, ' / ', obj_data.message);
+        }else{
+            // 그 외의 경우는 모두 메시지 목록에 저장.
+            messages.push(obj_data);
+
+            // 화면에서 구현 후 삭제할 로직 ▽
+            if(obj_data.infoType === Chat.infoType.alaram){
+                // 다른 유저의 출입 알림.
+                // 내가 입장 시엔 메시지가 나에게만 오고(issued), 내가 퇴장 시엔 clients 배열에서 삭제 후 전송되니까 infoType이 alaram인 경우는 무조건 다른 유저의 출입 알림.
+                console.log(`${obj_data.message}`);
+            }else{
+                if(obj_data.id === m_id){
+                    console.log(`나, ${obj_data.id}: ${obj_data.message}`);
+                }else{
+                    console.log(`남, ${obj_data.id}: ${obj_data.message}`);
+                }
             }
-        } else {  // - 입장이 아닌 경우는 채팅 메시지.
-            // 내가 보낸 메시지
-            if (obj_data.id === m_id) {
-                console.log(`나, ${obj_data.id}: ${obj_data.message}`);
-            } else {  // 남이 보낸 메시지
-                console.log(`남, ${obj_data.id}: ${obj_data.message}`);
-            }
+            // 화면에서 구현 후 삭제할 로직 △
         }
+
+        console.log('메시지 수: ', messages.length);
     });
 
     // message 전송.
@@ -49,11 +58,17 @@ client.connect(PORT, HOST, () => {
 
     client.on('close', () => {
         console.log('서버와의 연결이 끊겼습니다');
+
+        const disconnectAlaram = new Chat(m_id, '서버와의 연결이 끊겼습니다 !', Chat.infoType.alaram, false);
+        messages.push(disconnectAlaram);
+        console.log('메시지 수: ', messages.length);
     });
 });
 
 function sendMessage(m_id) {
-    const obj_chat = new Chat(m_id, inputContent.value, Chat.infoType.message, Chat.infoOrigin.self);
+    if(inputContent.value.length === 0) return;
+
+    const obj_chat = new Chat(m_id, inputContent.value, Chat.infoType.message, false);
     const json_chat = JSON.stringify(obj_chat);
     client.write(json_chat);
     inputContent.value = "";
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,21 +1,16 @@
 class Chat{
     // js는 생성자를 통해 전달된 매개변수를 동적으로 선언.
-    constructor(id, message, infoType, infoOrigin){
+    constructor(id, message, infoType, issued){
         this.id = id;
         this.message = message;
         this.infoType = infoType;
-        this.infoOrigin = infoOrigin;
+        this.issued = issued;
     }
 }
 
 Chat.infoType = Object.freeze({
     message: 'message',
     alaram: 'alaram'
-});
-
-Chat.infoOrigin = Object.freeze({
-    self: 'self',
-    others: 'others'
 });
 
 module.exports = Chat;
